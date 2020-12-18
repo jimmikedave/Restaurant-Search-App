8b601ef58f21227eb7f1307186ddc7a4f1c2ea08
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-12-18 11:12:46 -0500

    Configure search bar and add error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,43 @@
 import React, { useState } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [error, setError] = useState('');
+
+    // helper function
+    const searchApi = async (searchTerm) => {
+        // param ex. limit: 50 => /search?limit=50
+        try{
+            const response = await yelp.get('/search', {
+                params: {
+                    term: searchTerm,
+                    location: 'brooklyn',
+                    limit: 50 
+                }
+            });
+            setResults(response.data.businesses)
+        } catch (err) {
+            setError('Something went wrong')
+        }
+    };
+
+    // Call searchApi when component is first rendered **BAD CODE**
+    // searchApi('pasta')
+    // this will cause an infinite re-render
 
     return (
         <View>
             <SearchBar 
                 term={term} 
-                onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
             />
-            <Text>Search Screen</Text>
+            {error ? <Text>{error}</Text> : null}
+            <Text>We have found {results.length} results</Text>
             
         </View>
     )
