a83e8d720182f645172f20a36c536a708c8bfe82
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-12-18 10:20:32 -0500

    Configure axios to call yelp api

---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+    // root url we want to make the request to
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer pfswWigR9Zx1-qroTb80x3l2onjxh2XHScBN_smXGSoYvXAHbL2hAyQ0YUtnaFU-u6iOc9JRBM4-rTAhv8RksfuzS-WvFtijsMQ9orPgx4XlI0cVm6gmh5ZCP8HbX3Yx'
+    }
+});
